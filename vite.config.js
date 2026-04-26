1cdc73b7d2a4cd425947303ce5214eb40609a247
kauaferreiradonascimento627-cloud <kauaferreiradonascimento627@gmail.com>
2026-04-26 11:41:14 -0300

    vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,35 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import { VitePWA } from 'vite-plugin-pwa'
 
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    VitePWA({
+      registerType: 'autoUpdate',
+      includeAssets: ['favicon.ico'],
+      manifest: {
+        name: 'Barbearia O Vieira',
+        short_name: 'O Vieira',
+        description: 'Agende seu horário na Barbearia O Vieira',
+        theme_color: '#0c0a09',
+        background_color: '#0c0a09',
+        display: 'standalone',
+        orientation: 'portrait',
+        start_url: '/',
+        icons: [
+          {
+            src: 'https://i.imgur.com/BnR11UJ.png',
+            sizes: '192x192',
+            type: 'image/png'
+          },
+          {
+            src: 'https://i.imgur.com/BnR11UJ.png',
+            sizes: '512x512',
+            type: 'image/png'
+          }
+        ]
+      }
+    })
+  ],
 })
